bd228753921b8369d575a891172c31812684cb19
SamManoli975 <C23330671@mytudublin.ie>
2023-12-25 18:00:12 +0000

    created the selection sort algorithm and got it working fine using updateDisplay function and typical selection sort alogrithm

---
+++ js/index.js
@@ -0,0 +1,83 @@
+/*
+author: Sam Manoli
+description: main javascript file for the algorithm visualiser project
+*/
+newArr = document.getElementById("a_generate").addEventListener( "click",gen_array);
+sizeArr = document.getElementById("a_size").value;
+speedAlg = document.getElementById("a_speed").value;
+document.getElementById("a_size").addEventListener("input", update_rndArr);
+
+let cont=document.getElementById("array_container");
+
+var ButtonAlgs=document.querySelectorAll(".algs button");
+
+let divs = [];
+let rndArr =[];
+// let margin_size=0.1;
+//generate a new array
+function gen_array(){
+    // let divs = [];
+    // let rndArr =[];
+    cont.innerHTML="";
+
+
+    // console.log(sizeArr)
+    // console.log('Array generated');
+    
+    
+    for(var i = 0; i < sizeArr; i++){
+        
+        rndArr[i] = Math.floor(Math.random() * 80) + 10;
+        divs[i]=document.createElement("div");
+        cont.appendChild(divs[i]);
+        margin_size=0.1;
+        divs[i].style=" margin:0% " + margin_size + "%; background-color:purple; width:" + (100/sizeArr-(2*margin_size)) + "%; height:" + (rndArr[i]) + "%;";
+    }
+    //display array
+    // displayArray(arr);
+    console.log("Array generated: " + rndArr);
+}
+function update_rndArr()
+{   
+    sizeArr = document.getElementById("a_size").value;
+    gen_array();
+}
+
+window.onload=update_rndArr();
+
+for(var i=0;i<ButtonAlgs.length;i++)
+{
+    ButtonAlgs[i].addEventListener("click",sortAlgs);
+}
+// document.getElementById("a_selection").addEventListener("click", selection);
+
+function sortAlgs(){
+    this.classList.add("butt_selected");
+    switch(this.innerHTML)
+    {
+        // case "Bubble":Bubble();
+        //                 break;
+        case "Selection":selection();
+                        break;
+        // case "Insertion":Insertion();
+        //                 break;
+        // case "Merge":Merge();
+        //                 break;
+        // case "Quick":Quick();
+        //                 break;
+        // case "Heap":Heap();
+        //                 break;
+    }
+}
+speed = 900;//set the speed of the algorithm
+var delay_time=10000/(Math.floor(sizeArr/10)*speed);//Decrease numerator to increase speed.
+var c_delay=0;//This is updated ov every div change so that visualization is visible.
+
+
+//important function to edit the display of the block according the the sorting algorithm, i will have to place this
+//function strageically to edit the block
+function updateDisplay(cont, color, height){
+    window.setTimeout(function(){
+        cont.style=" margin:0% " + margin_size + "%; width:" + (100/sizeArr-(2*margin_size)) + "%; height:" + height + "%; background-color:" + color +  ";";
+    },c_delay+=delay_time);
+}
---
+++ js/selection.js
@@ -0,0 +1,66 @@
+/*
+author : Sam Manoli
+DESCRIPTION: javascript file to complete selection sort with the random algorithm and visualise it
+*/
+// rndArr = [1,5,2,6,4,3]
+
+
+console.log('this file is fine');
+
+//function to perform and update the selection sorting algorithm
+function selection(){
+
+    //going through the list
+    for (let i = 0; i < sizeArr; i++) {
+        //updating the current element red 
+        updateDisplay(divs[i],"red" , rndArr[i] );
+        //set the first element as the minimum
+        let min = i;
+        //going through the rest of the elements while still looking at i
+        for (let j = i + 1; j < sizeArr; j++) {
+            //updating all the other elements yellow as we go down the array
+            updateDisplay(divs[j],"yellow" , rndArr[j] );
+            //if the element we pass is smaller than the minimum element  then change it
+            if (rndArr[j] < rndArr[min]) {
+                //but if the min is not i, change the min to purple meaning back to normal colour
+                if(min != i){
+                    updateDisplay(divs[min],"purple" , rndArr[min] );
+                }
+                min = j;//set min
+                //change color to red meaning out of place 
+                updateDisplay(divs[min],"red" , rndArr[min] );
+            }
+            else{//or if the element is bigger than min set back to normal position aka unsorted
+
+                updateDisplay(divs[j],"purple" , rndArr[j] );
+            }
+        }
+        if (min !== i) {
+            //if min is not i swap the elements, so if the element we are looking at is not i
+            //then change the positions as we want the min to go before the other numbers
+            
+            // Swap
+            ;[rndArr[i], rndArr[min]] = [rndArr[min], rndArr[i]];
+
+            //change the height and color of either the min or the i element
+            updateDisplay(divs[min],"red",rndArr[min]);//Height update
+            updateDisplay(divs[i],"red",rndArr[i]);//Height update
+            updateDisplay(divs[min],"purple",rndArr[min]);//Color update
+        }
+        //after this the current elemnt in i has to be sorted so turn it green aka solved
+        updateDisplay(divs[i],"green" , rndArr[i] );
+
+    }
+    //turn it green also (this is the last element)
+    updateDisplay(divs[i],"green" , rndArr[i] );
+
+
+    //debugging checking the array
+    // for(i=0;i<sizeArr;i++){
+    //     console.log("Comparing: " + rndArr[i]);
+    // }
+    // return rndArr;
+    //more debugging
+    // console.log("Selection sort completed: " + rndArr)
+};
+
