3222494d828b93bbe1401f66898f3bb457684013
SamManoli975 <C23330671@mytudublin.ie>
2023-12-26 18:50:42 +0000

    changed shell.js a small bit and added comb.js sorting algorithm and visualised it

---
+++ js/comb.js
@@ -0,0 +1,68 @@
+/*
+author: sam manoli
+description: comb alogrithm and also visualising it using the updateDisplay function
+
+
+*/
+
+function getNextGap(gap){
+        
+        gap = parseInt((gap*10)/13, 10);
+        if (gap < 1)
+            return 1;
+        return gap;
+    }
+  
+
+function comb(){
+
+    
+    let gap = sizeArr;
+
+    
+    let swapped = true;
+
+   
+    while (gap != 1 || swapped == true){
+        
+        gap = getNextGap(gap);
+
+        
+        swapped = false;
+
+        
+        for (let i=0; i<sizeArr-gap; i++){
+            let currentindex = i;
+
+            // updateDisplay(divs[currentindex],"yellow",rndArr[currentindex]);
+
+            if (rndArr[i] > rndArr[i+gap]){
+
+                // updateDisplay(divs[i],"yellow",rndArr[i]);
+
+                updateDisplay(divs[i+gap],"red",rndArr[i+gap]);
+                updateDisplay(divs[i],"red",rndArr[i]);
+
+
+                
+                let temp = rndArr[i];
+                rndArr[i] = rndArr[i+gap];
+                rndArr[i+gap] = temp;
+
+                updateDisplay(divs[i],"purple",rndArr[i]);
+                updateDisplay(divs[i+gap],"purple",rndArr[i+gap]);
+
+                // updateDisplay(divs[i],"purple",rndArr[i]);
+                updateDisplay(divs[i],"green",rndArr[i]);
+                updateDisplay(divs[i+gap],"green",rndArr[i+gap]);
+                swapped = true;
+
+            }
+                
+        }
+        updateDisplay(divs[i],"green",rndArr[i]);
+
+    }
+    console.log(rndArr);
+}
+    
--- js/shell.js
+++ js/shell.js
@@ -4,53 +4,62 @@ description: shell sort visualiser
 
 */
 
-function shell() { 
+function shell(){ 
     c_delay=0;
      
    
-        // Start with a big gap, then reduce the gap 
-        for (let gap = Math.floor(sizeArr/2); gap > 0; gap = Math.floor(gap/2)) 
-        { 
-          
-            // Do a gapped insertion sort for this gap size. 
-            // The first gap elements a[0..gap-1] are already 
-            // in gapped order keep adding one more element 
-            // until the entire rndArray is gap sorted 
-            for (let i = gap; i < sizeArr; i += 1) 
-            { 
-                updateDisplay(divs[i],"yellow",rndArr[i]);
-                // add a[i] to the elements that have been gap 
-                // sorted save a[i] in temp and make a hole at 
-                // position i 
-                let temp = rndArr[i]; 
-                
-                // shift earlier gap-sorted elements up until 
-                // the correct location for a[i] is found 
-                let j; 
-                // updateDisplay(divs[i],"red",rndArr[i]);
+    // Start with a big gap, then reduce the gap 
+    for (let gap = Math.floor(sizeArr/2); gap > 0; gap = Math.floor(gap/2)){ 
+        
+        // Do a gapped insertion sort for this gap size. 
+        // The first gap elements a[0..gap-1] are already 
+        // in gapped order keep adding one more element 
+        // until the entire rndArray is gap sorted 
+        for (let i = gap; i < sizeArr; i += 1) { 
+            // add a[i] to the elements that have been gap 
+            // sorted save a[i] in temp and make a hole at 
+            // position i 
+            let temp = rndArr[i]; 
+            let currentindex = i;
 
-                for (j = i; j >= gap && rndArr[j - gap] > temp; j -= gap) {
-                    updateDisplay(divs[j-gap],"red",rndArr[j-gap]);
-                    rndArr[j] = rndArr[j - gap]; 
-                    
-                }
-                    
+            updateDisplay(divs[currentindex],"yellow",rndArr[currentindex]);
+            // updateDisplay(divs[i],"purple",rndArr[i]);
+ 
+            // shift earlier gap-sorted elements up until 
+            // the correct location for a[i] is found 
+            let j; 
+            // updateDisplay(divs[i],"red",rndArr[i]);
 
+            for (j = i;j >= gap && rndArr[j - gap] > temp;j -= gap){
 
-                // put temp (the original a[i]) in its correct 
-                // location 
-                rndArr[j] = temp; 
-                updateDisplay(divs[j-gap],"green",rndArr[j-gap]);
-                updateDisplay(divs[j],"green",rndArr[j]);
-                
+                updateDisplay(divs[j],"red",rndArr[j]);
+                updateDisplay(divs[j-gap],"red",rndArr[j-gap]);
 
-            } 
-            
+                // updateDisplay(divs[j-gap],"red",rndArr[j-gap]);
+                rndArr[j] = rndArr[j - gap]; 
+
+                updateDisplay(divs[j],"purple",rndArr[j]);
+                updateDisplay(divs[j-gap],"purple",rndArr[j-gap]);
+
+                // updateDisplay(divs[j],"purple",rndArr[j]);
+                // updateDisplay(divs[j-gap],"purple",rndArr[j-gap]);   
+            }      
+            // put temp (the original a[i]) in its correct 
+            // location 
+            updateDisplay(divs[j],"red",rndArr[j]);
+            rndArr[j] = temp; 
+            updateDisplay(divs[currentindex],"green",rndArr[currentindex]);
+            updateDisplay(divs[j],"green",rndArr[j]);
+
+            // updateDisplay(divs[i],"green",rndArr[i]);
         } 
-        for(let k =0;k<sizeArr;k++){
-            updateDisplay(divs[k],"green",rndArr[k]);
-        }
-        updateDisplay(divs[0],"green",rndArr[0]);
-        console.log(rndArr);
-        return rndArr; 
+        
+    } 
+    for (let i = 0; i < sizeArr; i++) {
+        updateDisplay(divs[i], "green", rndArr[i]);
+    }
+    
+    
+    console.log(rndArr);
+    return rndArr; 
 } 
